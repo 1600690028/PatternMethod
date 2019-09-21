216ccd9625a6b18379ab0c472429d10d87541734
FreoStudio <1600690028@qq.com>
2019-09-21 20:48:27 +0800

    抽象工厂模式 单实例设计模式

--- Singleton/src/com/freo/design/model/SingletonPatternDemo.java
+++ Singleton/src/com/freo/design/model/SingletonPatternDemo.java
@@ -11,4 +11,16 @@ package com.freo.design.model;
  * ***************************
  */
 public class SingletonPatternDemo {
+    public static void main(String[] args) {
+
+        //illegal construct
+        //Compile Time Error: The constructor SingleObject() is not visible
+        //SingleObject object = new SingleObject();
+
+        //Get the only object available
+        SingleObject object = SingleObject.getInstance();
+
+        //show the message
+        object.showMessage();
+    }
 }
